3f6702aea51d092ca980f7b0faa2d49b90770831
Fadel Akram <af8356207@gmail.com>
2025-10-14 09:41:12 +0100

    rmoved lab2/lab

--- labs/lab2/lab/src/main/java/com/example/snowball/SnowballStemmer.java
+++
@@ -1,6 +0,0 @@
-package com.example.snowball;
-import java.lang.reflect.InvocationTargetException;
-
-public abstract class SnowballStemmer extends SnowballProgram {
-    public abstract boolean stem();
-};
