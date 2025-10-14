0166319b572b2eee5e4deedd9471c0f5e3c4ffcf
Fadel Akram <af8356207@gmail.com>
2025-10-14 09:41:33 +0100

    Squashed 'labs/lab2/lab/' content from commit 0a47fda
    git-subtree-dir: labs/lab2/lab
    git-subtree-split: 0a47fda79c5ee2dde9c9f6dc4163a48a5fde22e9

---
+++ src/main/java/com/example/Posting.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Posting {
+    int docId;
+    int tf;
+
+    public Posting(int docId, int termFrequency) {
+        this.docId = docId;
+        this.tf = termFrequency;
+    }
+}
