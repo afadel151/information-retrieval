6cc7b780b5af62e136468266a6db030bd6ad084a
Fadel Akram <af8356207@gmail.com>
2025-11-05 12:33:35 +0100

    set up rank-based clas

--- labs/lab3/src/main/java/com/emp/ir/RankBasedHelper.java
+++ labs/lab3/src/main/java/com/emp/ir/RankBasedHelper.java
@@ -1,5 +1,44 @@
 package com.emp.ir;
 
+import java.io.IOException;
+import java.util.Map;
+
 public class RankBasedHelper {
-    
+
+    public static void main(String[] args) throws IOException {
+        String folderPath = "results";
+        int k = 5;
+        Map<String, Map<Integer, QueryResult>> allResults = ResultParser.parseAllSystems(folderPath);
+        String assessmentsPath = "assessments.txt";
+        Map<Integer, QueryAssessment> assessments = AssessmentParser.parseAssessments(assessmentsPath);
+        for (var systemNumber : allResults.keySet()) {
+            System.out.println("System ->" + systemNumber);
+            for (var queryId : allResults.get(systemNumber).keySet()) {
+                QueryResult queryResults = allResults.get(systemNumber).get(queryId);
+                QueryAssessment queryAssessments = assessments.get(queryId);
+
+                System.out.println("Query:" + queryId);
+                System.out.println("Recall :" + calculateRecallAtK(k, queryResults, queryAssessments));
+                System.out.println("Precision :" + calculatePrecisionAtK(k, queryResults, queryAssessments));
+                System.out.println("F1-score :" + calculateF1AtK(k, queryResults, queryAssessments));
+            }
+        }
+
+    }
+
+    public static double calculateRecallAtK(int K, QueryResult result, QueryAssessment assessment) {
+        double x = 0;
+        return x;
+
+    }
+
+    public static double calculatePrecisionAtK(int K, QueryResult result, QueryAssessment assessment) {
+        double x = 0;
+        return x;
+    }
+
+    public static double calculateF1AtK(int K, QueryResult result, QueryAssessment assessment) {
+        double x = 0;
+        return x;
+    }
 }
